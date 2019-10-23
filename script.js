4d36db532a751dc687e3ff6164cdd66b9af69fbc
zahidi14 <d.zahidi1412@gmail.com>
2019-10-23 18:05:57 +0700

    responsive design fix

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function selector(evt, selectTab){
    var i, x, tablinks;
    x= document.getElementsByClassName("select");
@@ -6,8 +8,34 @@ function selector(evt, selectTab){
    }
    tablinks = document.getElementsByClassName("tablink");
    for(i=0;i<x.length;i++){
-       tablinks[i].className = tablinks[i].className.replace(" ","");
+       tablinks[i].className = tablinks[i].className.replace(" active","");
+       console.log("cok");
    }
-   document.getElementById(selectTab).style.display = "block";
-   evt.currentTarget.firstElementChild.className +=" ";
+   if(window.matchMedia("(max-width: 800px)")){
+    document.getElementById(selectTab).style.display = "block";
+    console.log("flex");
+   }else{
+    document.getElementById(selectTab).style.display = "flex";
+    console.log("block");
+   }
+   
+   evt.currentTarget.className +=" active";
+}
+
+function collap(){
+  var coll = document.getElementsByClassName("colapse-quest");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      console.log(this);
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 }
